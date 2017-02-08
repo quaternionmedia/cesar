bde4132adcfb4f4fd7e62135f6db81d56bc6ae98
Peter <peter.kagstrom@gmail.com>
2017-02-07 16:58:18 -0800

    lots more cleanup

--- config.py
+++
@@ -1,7 +0,0 @@
-SECRET_KEY = 'this is a secret key. There are others like it, but this one is mine'
-STORMPATH_API_KEY_ID = '6RHWJHH7RBRENKGHAEUKKARZH'
-STORMPATH_APPLICATION = 'app'
-STORMPATH_API_KEY_SECRET = '2Agty3uZzgHJZWGOAJXwtiE6vfBlXKfiGjt/b1BsTSs'
-STORMPATH_ENABLE_REGISTRATION = False
-UPLOAD_FOLDER = '/george/scripts'
-MAX_CONTENT_PATH = 99999
