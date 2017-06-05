37b65f8019bbf30ad1b022150e33f2b25640d6e0
Harpo <harpo@harpo.me>
2017-06-05 10:35:10 -0700

    almost full nextcloud install

--- install.sh
+++ install.sh
@@ -10,17 +10,17 @@ done
 
 # python install
 
-wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tgz
-tar xvzf Python-3.6.1.tgz
-rm Python-3.6.1.tgz
-cd Python-3.6.1
-./configure --enable-optimizations
-make
-make altinstall
-cd ..
-
-PIPS="hug jinja2 pillow"
-for PIP in $PIPS
-do
-	pip3.6 install $PIP
-done
+# wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tgz
+# tar xvzf Python-3.6.1.tgz
+# rm Python-3.6.1.tgz
+# cd Python-3.6.1
+# ./configure --enable-optimizations
+# make
+# make altinstall
+# cd ..
+#
+# PIPS="hug jinja2 pillow"
+# for PIP in $PIPS
+# do
+# 	pip3.6 install $PIP
+# done
