f3c3cf61d9b3cf33da411801fec2ffe755f81396
Peter <peter.kagstrom@gmail.com>
2017-10-02 20:49:34 -0700

    george and gromit heartbeat dev

--- george_heartbeat.py
+++ george_heartbeat.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import datetime
+import shutil
 
 home = '//Users/peterkagstrom/Dropbox/george'
 
@@ -9,26 +10,25 @@ def touchDirectory(path):
 		os.mkdir(path)
 
 def traverseDirectory(rootDir='//'):
-	logging.debug('Starting directory traverse on ' + rootDir)
+	logging.info('Starting directory traverse on ' + rootDir)
 	for dirpath, dirnames, filenames in os.walk(rootDir):
-		logging.debug('Found directory: %s' % dirpath)
-		logging.debug(str(dirpath) + " consumes ")
-		logging.debug(str(sum(os.path.getsize(os.path.join(dirpath, name)) for name in filenames)))
-		logging.debug('bytes in ' + str(len(filenames)) +
-		' non-directory files')
+		logging.debug('Found directory: %s' % dirpath + ', and it consumes ' + str(sum(os.path.getsize(os.path.join(dirpath, name)) for name in filenames)) + ' bytes in ' + str(len(filenames)) + ' non-directory files')
 		for fname in filenames:
 			logging.debug('\t%s' % fname)
+
+def copyFile(src,dest):
+	shutil.copyfile(src,dest)
 
 if __name__ == '__main__':
 	#run system and network discovery
 	#
 	#System Discovery
 	touchDirectory(home)
-	#setup logging
+	#setup logging (logging.debug, info, warning, error, critical)
 
 	logpath = home + '/glogs'
 	touchDirectory(logpath)
-	logname = logpath + '/' + str(datetime.datetime.now())
+	logname = logpath + '/' + str(datetime.datetime.now()) + '.glog'
 
 	logging.basicConfig(filename=logname, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
 
